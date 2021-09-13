56b26ed55e441ead271fde06601e81b263a75781
Luis Cossío <luis.cossio@outlook.com>
2021-09-12 21:10:11 -0500

    added plotting, mutation, city trading,  markov chains, initialization of temperature...

--- SimAnnealing.py
+++ SimAnnealing.py
@@ -1,6 +1,9 @@
 from math import exp
+
+import matplotlib.pyplot as plt
 import numpy as np
 from numpy.random import random
+
 from TSP import TSP
 
 KB = 1.380649e-23
@@ -9,7 +12,16 @@ class SimAnnealing:
     '''
     @author: Luis Cossio
     '''
-    def __init__(self, n_iterations=500, init_temp=0.1, min_temp=0.000000001, alpha=0.1, beta=1.5, n_cities=10, n_dims=2):
+    def __init__(self, 
+                 n_iterations=50000, 
+                 first_temp=0.1, 
+                 min_temp=0.0001, 
+                 alpha=0.0001, 
+                 beta=1.01, 
+                 n_cities=30, 
+                 n_travelers=3, 
+                 trade_cities=False, 
+                 shortest_travel=4):
         if not(0 < alpha < 1):
             raise AttributeError(f'alpha must be in within the range (0,1). Currently alpha={alpha}')
         if beta <= 1:
@@ -17,35 +29,68 @@ class SimAnnealing:
         self.alpha = alpha
         self.beta = beta
         self.n_cities = n_cities
-        self.n_dims = n_dims
         self.n_iter = n_iterations
-        self.temperature = init_temp
         self.min_temp = min_temp
-        self.tsp = TSP(n_cities=n_cities, n_dimensions=n_dims)
-        self.solution = self.random_solution()
+        self.tsp = TSP(n_cities=n_cities, 
+                        n_travelers=n_travelers, 
+                        trade_cities=trade_cities, 
+                        shortest_travel=shortest_travel)
+        self.solution = self.tsp.random_solution()
         self.fitness = self.tsp.fitness(self.solution)
+        self.temperature = self.init_temp(first_temp=first_temp)
+        self.best_solutions = []
+        self.best_fitnesses = []
 
+    def cadena_markov(self, L0=30):
+        accepted = 0
+        for _ in range(L0):
+            candidate = self.tsp.mutate(self.solution)
+            cand_fit = self.tsp.fitness(candidate)
+            if cand_fit < self.fitness or random() < self.p_acceptance(cand_fit):
+                self.solution, self.fitness = candidate, cand_fit
+                accepted += 1
+        return accepted / L0
 
-    def random_solution(self):
-        return np.random.choice(self.n_cities, self.n_cities, replace=False)
+    def init_temp(self, first_temp=0.1, L0=30, expected_p=0.5):
+        p = 0
+        self.temperature = first_temp
+        while p < expected_p:
+            p = self.cadena_markov(L0)
+            self.temperature *= self.beta
+        return self.temperature
 
     def p_acceptance(self, candidate_fitness):
-        return exp(-abs(self.fitness - candidate_fitness) / self.temperature)
+        return np.exp(-abs(self.fitness - candidate_fitness) / self.temperature)
 
     def metropolis(self, candidate):
         cand_fit = self.tsp.fitness(candidate)
-        if cand_fit < self.fitness or random.random() < self.p_acceptance(cand_fit):
+        if cand_fit < self.fitness or random() < self.p_acceptance(cand_fit):
             return candidate, cand_fit
         return self.solution, self.fitness
 
-    def run(self):
-        while self.temperature > self.min_temp:
-            candidate = self.tsp.mutate(self.solution)
-            self.solution, self.fitness = self.metropolis(candidate)
-            self.best_fitnesses.append(self.fitness)
-            self.temperature *= self.alpha
+    def update_temp(self):
+        self.temperature *= 1-self.alpha #(self.best_fitnesses[-2] / self.best_fitnesses[-1])
 
-            
-    def get_temp(self):
-        self.temperature *= self.alpha
-        return self.temperature
+    def run(self):
+        i = 0
+        while self.temperature > self.min_temp and i < self.n_iter:
+            self.cadena_markov(L0=1)
+            self.best_fitnesses.append(self.fitness)
+            self.best_solutions.append(self.solution)
+            self.update_temp()
+            i += 1
+
+    def plot_solution(self):
+        self.tsp.plot_solution(self.solution)
+
+if __name__ == '__main__':
+    sa = SimAnnealing()
+    sa.run()
+
+    plt.xlabel('iteraciones')
+    plt.ylabel('menor encontrado')
+    plt.plot(range(len(sa.best_fitnesses)), sa.best_fitnesses)
+
+    plt.show()
+
+    sa.plot_solution()
--- TSP.py
+++ TSP.py
@@ -1,10 +1,14 @@
 import numpy as np
 from functools import lru_cache
-
+from itertools import accumulate
+import matplotlib.pyplot as plt
 class TSP:
-    def __init__(self, n_cities=10, n_dimensions=2):
+    def __init__(self, n_travelers=3, n_cities=50, n_dimensions=2, trade_cities=False, shortest_travel=3):
         self.n_cities = n_cities
         self.n_dimensions = n_dimensions
+        self.n_travelers = n_travelers
+        self.trade_cities = trade_cities
+        self.shortest_travel = shortest_travel
         self.cities = np.random.random_sample(size=(n_cities, n_dimensions))
 
     @lru_cache(maxsize=1000)
@@ -14,27 +18,78 @@ class TSP:
         distance = euclidian_distance(o, d)
         return distance
     
+    def random_solution(self):
+        sol = np.random.choice(self.n_cities, self.n_cities, replace=False)
+        step_size = self.n_cities//self.n_travelers
+        return np.split(sol, range(step_size, step_size*self.n_travelers, step_size))
 
     def fitness(self, solution):
         fitness = 0
-        for i in range(self.n_cities):
-            fitness += self.dist(solution[i-1], solution[i])
+        for travel in solution:
+            travel = np.asarray(travel)
+            for i in range(np.size(travel)):
+                fitness += self.dist(travel[i-1], travel[i])
         return fitness
     
     def mutate(self, solution):
+        split_indices = list(accumulate([np.size(x) for x in solution]))[:-1]
+        conc_solution = np.concatenate(solution)
+
         # Shift
         shift = np.random.randint(self.n_cities)
-        mutation = np.roll(solution,shift)
+        mutation = np.roll(conc_solution,shift)
 
         # Mutate
-        i = np.random.randint(self.n_cities)
-        l = i + np.random.randint(i, self.n_cities)
+        i = np.random.randint(self.n_cities-2)
+        l = np.random.randint(low=i + 2, high=i+2+self.n_cities//2)
+        l = l if l < self.n_cities else self.n_cities
         mutation[i:l] = np.random.choice(mutation[i:l], l-i, replace=False)
 
         # Unshift
         mutation = np.roll(mutation, -shift)
+
+        # trade cities between travelers
+        if self.trade_cities:
+            j = np.random.randint(1,len(split_indices)+1)
+            delta = np.random.randint(-1, 1)
+            split_indices = [0] + split_indices + [self.n_cities]
+            if split_indices[j-1] + self.shortest_travel-1 < split_indices[j] + delta < split_indices[j+1] - self.shortest_travel-1:
+                split_indices[j] += delta
+            split_indices = split_indices[1:-1]
+
+        # resplit
+        mutation = np.split(mutation, split_indices)
         return mutation
 
+    def plot_solution(self, solution):
+        positions = self.cities
+        fig, ax = plt.subplots(1, 2, sharex=True, sharey=True)         # Prepare 2 plots
+        ax[0].set_title('Raw nodes')
+        ax[1].set_title('Optimized tour')
+        ax[0].scatter(positions[:, 0], positions[:, 1])             # plot A
+        ax[1].scatter(positions[:, 0], positions[:, 1])             # plot B
+        
+        # Draw arrows
+        for travel in solution:
+            travel = np.asarray(travel)
+            c = list(np.random.rand(3))
+            for i in range(np.size(travel)):
+                start_pos = positions[travel[i-1]]
+                end_pos = positions[travel[i]]
+                ax[1].annotate("",
+                        xy=start_pos, xycoords='data',
+                        xytext=end_pos, textcoords='data',
+                        arrowprops=dict(arrowstyle="->",
+                                        connectionstyle="arc3",
+                                        color=c))
+        # Info box
+        textstr = "N nodes: %d\nTotal length: %.3f" % (self.n_cities, self.fitness(solution))
+        props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+        ax[1].text(0.05, 0.95, textstr, transform=ax[1].transAxes, fontsize=9, # Textbox
+                verticalalignment='top', bbox=props)
+
+        plt.tight_layout()
+        plt.show()
 
 def euclidian_distance(a, b):
     a = np.asarray(a)
